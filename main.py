083a8753ea2c4346d99950848bc72a799d2f4e9c
victoire-courtilly <victoire.courtilly@edu.esiee.fr>
2025-11-13 09:06:39 +0000

    message expliquatif

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ from math import sqrt
 def isprime(p):
 
     # votre code ici
+    if p <= 1 :
+        return False
+    elif p == 2 :
+        return True
+    elif p % 2 == 0 :
+        return False
 
-    pass
+    for i in range(3, int(sqrt(p))+1, 2) :
+        if p%i == 0 :
+            return False
+    return True
 
 #### Fonction principale
 
